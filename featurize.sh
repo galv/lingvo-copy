3b7b6cd73050605bbd86f0f3bdbcacadcf567949
anjaligopi <anjaligopi23@gmail.com>
2020-11-04 18:03:39 +0000

    wip

--- featurize.sh
+++ featurize.sh
@@ -10,13 +10,13 @@ function launch_feat() {
         --generate_tfrecords \
         --input_tarball=${input_tarball} \
         --input_text=${input_tarball/tar.gz/csv} \
-        --num_shards ${num_proc_shards} \
+        --num_shards ${num_output_shards} \
         --shard_id ${shard_id} \
-        --output_range_begin 0 \
-        --output_range_end ${num_output_shards} \
+        --output_range_begin ${shard_id} \
+        --output_range_end $((shard_id + 1)) \
         --num_output_shards ${num_output_shards} \
         --transcripts_filepath "${output_dir}.txt" \
-        --output_template "${output_dir}.tfrecords-%5.5d-of-%5.5d"
+        --output_template "${output_dir}.tfrecords-%5.5d-of-%5.5d" 2>&1 | tee feat.${shard_id}.log
 }
 export -f launch_feat
 
@@ -24,16 +24,16 @@ export input_tarball="${1}/development.tar.gz"
 export output_dir="${output_base}/${2}/devtest/dev"
 export num_proc_shards=1
 export num_output_shards=1
-launch_feat 0
+# launch_feat 0
 
 export input_tarball="${1}/test.tar.gz"
 export output_dir="${output_base}/${2}/devtest/test"
 export num_proc_shards=1
 export num_output_shards=1
-launch_feat 0
+# launch_feat 0
 
 export input_tarball="${1}/train.tar.gz"
 export output_dir="${output_base}/${2}/train/train"
 export num_proc_shards=${3:-16}
 export num_output_shards=512
-# parallel -j $num_proc_shards launch_feat ::: $(seq 0 $num_proc_shards)
+parallel --citation -j $num_proc_shards launch_feat ::: $(seq 0 $num_output_shards)
