c8fa35b6e658549210e62f9c9207c901308b8883
Daniel Galvez <dt.galvez@gmail.com>
2021-01-28 15:05:05 -0500

    WIP Vimeo scraper for sake of sharing.
    Not robust.

--- scripts/vimeo/get_subtitled_cc_videos.py
+++ scripts/vimeo/get_subtitled_cc_videos.py
@@ -3,7 +3,7 @@ from itertools import product
 import json
 import os
 import sys
-from threading import Thread
+from threading import Lock, Thread
 import time
 
 
@@ -24,15 +24,17 @@ NUM_THREADS = 16
 class VimeoPageBeyond100Error(ValueError):
   pass
 
+# {'error': 'An error has occurred while searching for videos. Please try again.', 'link': None, 'developer_message': 'Search is currently unavailable. Please retry your request.', 'error_code': 7300}
 def robust_request(request_url):
   while True:
     next_response = requests.get(request_url, headers=HEADERS)
     next_response_json = next_response.json()
-    if next_response_json.get("error_code") == 9000:
+    if next_response_json.get("error_code") in (9000, 7300):
       # We are being rate limited. The quota will reset after 60 seconds
+      # 7300 means that the search service is unavailable at the moment, meanwhile.
       time.sleep(5)
       continue
-    elif next_response_json.get("error_code") == 2204:
+    elif next_response_json.get("error_code") in (2204, 2969):
       raise VimeoPageBeyond100Error()
     else:
       return next_response
@@ -83,21 +85,45 @@ def run_wrapper(query_license):
   run(query, license)
 
 def run(query, license):
-  os.makedirs("output_dir", exist_ok=True)
-  with open(f"output_dir/{query}_{license}.txt", "w") as fh:
-    for datum in tqdm.tqdm(VideoMetadataIterator(license, query), desc=f"{query}_{license}"):
-      uri = datum["uri"]
-      duration = datum["duration"]
-      fh.write(f"{uri},{duration}\n")
+  for datum in VideoMetadataIterator(license, query):
+    with LOCK:
+      item_to_duration[datum["uri"]] = datum["duration"]
+
+  # with open(f"output_dir/{query}_{license}.txt", "w") as fh:
+  #   for datum in tqdm.tqdm(VideoMetadataIterator(license, query), desc=f"{query}_{license}"):
+  #     uri = datum["uri"]
+  #     duration = datum["duration"]
+  #     fh.write(f"{uri},{duration}\n")
 
 # https://vimeo.com/creativecommons/cc0/: 10 * 11_143 videos
 # https://vimeo.com/creativecommons/by/: 10 * 161_770 videos
 
+# https://docs.python.org/dev/library/functions.html#chr
+LOCK = Lock()
+item_to_duration = {}
+
+def check_size():
+  while True:
+    with LOCK:
+      total_count = len(item_to_duration)
+    print("Total count:", total_count)
+    print("CC-BY count:", 10 * 161_770)
+    # This may be 1941168 instead...
+    # 1617700
+    # 1941168
+    with open('cc_by_videos.json', 'w') as fh, LOCK:
+      json.dump(item_to_duration, fh)
+    time.sleep(10)
+
 with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
   # Note: The query string is case-insensitive, so no need for capital letters
-  pairs = list(product("abcdefghijklmnopqrstuvwxyz",
-                       ["CC-BY", "CC0"],))
+  # pairs = list(product("abcdefghijklmnopqrstuvwxyz",
+  #                      ["CC-BY", "CC0"],))
+  pairs = list(product(map(lambda i: chr(i), range(0, 1_114_111 + 1)), ["CC-BY"]))
+  thread = Thread(target=check_size)
+  thread.start()
   list(tqdm.tqdm(executor.map(run_wrapper, pairs), total=len(pairs), desc="Total"))
+thread.join()
 
 # threads = []
 # for query, license in product("abcdefghijklmnopqrstuvwxyz",
